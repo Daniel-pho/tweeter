88c91da70f972326840d1f223053894cced70c76
Daniel Pho <daniel.phou@hotmail.com>
2024-02-28 09:35:31 -0500

    added and linked script file for a character counter in the textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+console.log("Composer character counter script loaded successfully.");
+
+$(document).ready(function() {
+  $('.new-tweet textarea').on('keypress', function() {
+    
+    const text = $(this).val();
+    const counter = $(this).closest(".new-tweet").find(".counter");
+    const textLength = text.length;
+    const maxChars = 140 - textLength;
+    counter.text(maxChars)
+
+    if (maxChars <= 0) {
+     $(counter).css("color", "red");
+    } else if (maxChars >= 0) {
+      $(counter).css("color", "");
+    }
+  
+});
+}) 
