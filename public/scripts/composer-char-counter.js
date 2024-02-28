103ca02be52fbb0dce8c5b239b0fceebf1ed96c4
Daniel Pho <daniel.phou@hotmail.com>
2024-02-28 18:46:29 -0500

    added tweet container to main body

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 console.log("Composer character counter script loaded successfully.");
 
 $(document).ready(function() {
-  $('.new-tweet textarea').on('keypress', function() {
+  $('.new-tweet textarea').on('keyup', function() {
     
     const text = $(this).val();
     const counter = $(this).closest(".new-tweet").find(".counter");
